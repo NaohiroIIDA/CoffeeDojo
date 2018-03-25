8c28d4c90a9d4d806e4d6d9ef111846b48356865
Naohiro iida <nao@nekotorobot.com>
2018-03-25 11:21:42 +0900

    fix files

--- appTest/debug/Libraries/EMlib/3rd-party/EMLib/usb/src/em_usbdint.d
+++
@@ -1,2 +0,0 @@
-/Users/naohiroiida/XDK-Workspace/appTest/debug/objects/./../Libraries/EMlib/3rd-party/EMLib/usb/src/em_usbdint.o: \
- ../Libraries/EMlib/3rd-party/EMLib/usb/src/em_usbdint.c
--- appTest/debug/objects/source/cJSON.d
+++
@@ -1,4 +0,0 @@
-/Users/naohiroiida/XDK-Workspace/appTest/debug/objects/source/cJSON.o: \
- source/cJSON.c ../Common/include/cJSON.h
-
-../Common/include/cJSON.h:
